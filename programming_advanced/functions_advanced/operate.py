98f337adf6a619acef4c6151f9128235e87ce4ea
VR86-web <145848782+VR86-web@users.noreply.github.com>
2024-01-26 17:35:49 +0100

    exercise added

---
+++ programming_advanced/functions_advanced/operate.py
@@ -0,0 +1,17 @@
+from functools import reduce
+
+
+def operate(operator, *args):
+    if operator == "+":
+        return reduce(lambda x, y: x + y, args)
+    elif operator == "-":
+        return reduce(lambda x, y: x - y, args)
+    elif operator == "*":
+        return reduce(lambda x, y: x * y, args)
+    elif operator == "/":
+        return reduce(lambda x, y: x / y, args)
+
+
+# solution 2
+# def operate(operator, *args):
+# return reduce(lambda x, y: eval(f"{x}{operator}{y}"), args)
