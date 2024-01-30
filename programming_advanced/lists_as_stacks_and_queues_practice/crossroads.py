8a50e7907d5717085c288512a7d500faca34d2d8
VR86-web <145848782+VR86-web@users.noreply.github.com>
2024-01-30 17:36:58 +0100

    exercise added

---
+++ programming_advanced/lists_as_stacks_and_queues_practice/crossroads.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+green_light_duration = int(input())
+free_window_duration = int(input())
+
+
+while True:
+    command = input()
+    if command == "END":
+        break
