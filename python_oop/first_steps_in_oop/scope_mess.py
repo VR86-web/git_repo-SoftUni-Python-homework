6f80388a67f44896056ba7513f51ef91702d584a
VR86-web <145848782+VR86-web@users.noreply.github.com>
2024-02-21 23:06:59 +0100

    changes added

--- python_oop/first_steps_in_oop/scope_mess.py
+++ python_oop/first_steps_in_oop/scope_mess.py
@@ -5,16 +5,18 @@ def outer():
     x = "local"
 
     def inner():
+        nonlocal x
         x = "nonlocal"
         print("inner:", x)
 
     def change_global():
+        global x
         x = "global: changed!"
 
-        print("outer:", x)
-        inner()
-        print("outer:", x)
-        change_global()
+    print("outer:", x)
+    inner()
+    print("outer:", x)
+    change_global()
 
 
 print(x)
