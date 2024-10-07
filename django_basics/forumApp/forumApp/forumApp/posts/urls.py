d14ab2ad6d193c1b0ded8fbc9dcd29e6e05ede6a
VR86-web <145848782+VR86-web@users.noreply.github.com>
2024-10-07 23:19:24 +0200

    added more solutions

--- django_basics/forumApp/forumApp/forumApp/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from forumApp.posts import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('dashboard/', views.dashboard, name='dash'),
-]
-
