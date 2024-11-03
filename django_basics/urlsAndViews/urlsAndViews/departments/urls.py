db3aed01198b6ac5b54fd202cacdbc7cdf9a034d
VR86-web <145848782+VR86-web@users.noreply.github.com>
2024-11-03 22:57:36 +0100

    added more views

--- django_basics/urlsAndViews/urlsAndViews/departments/urls.py
+++ django_basics/urlsAndViews/urlsAndViews/departments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from urlsAndViews.departments.views import index, view_with_name, view_with_int_pk
+from urlsAndViews.departments.views import index, view_with_name, view_with_int_pk, view_with_slug
 
 urlpatterns = [
     path('', index, name='index'),
     path('<int:pk>/', view_with_int_pk),
+    path('<slug:slug>/', view_with_slug),
     path('<param>/', view_with_name),
 ]
--- django_basics/urlsAndViews/urlsAndViews/departments/views.py
+++ django_basics/urlsAndViews/urlsAndViews/departments/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from urlsAndViews.departments.models import Department
+
+
 # Create your views here.
 
 
@@ -14,6 +17,14 @@ def view_with_name(request, *args, **kwargs):
 
 def view_with_int_pk(request, pk: int):
     return HttpResponse(f'Primary Key: {pk}')
+
+
+def view_with_slug(request, slug):
+    department = Department.objects.get(slug=slug)
+    return HttpResponse(f"Department from slug: {department}")
+
+
+
 
 
 
